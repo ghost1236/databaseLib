2d36ad0651b3e18b827b18852cb77f5c5ce4d89e
ghost1236 <ghost1236@thelittlecat.kr>
2023-04-11 10:24:34 +0900

    수정 내용 1.  매니페스트 수정

--- app/src/main/java/kr/smiling/smdatabase/MainActivity.kt
+++ app/src/main/java/kr/smiling/smdatabase/MainActivity.kt
@@ -2,10 +2,13 @@ package kr.smiling.smdatabase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import net.common.databaselib.SQLManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
