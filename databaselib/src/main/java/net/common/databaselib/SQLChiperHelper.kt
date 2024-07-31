04f064d65a944171b295d8296486ffa723110670
ghost1236 <>
2024-07-31 14:21:29 +0900

    리스너 수정

--- databaselib/src/main/java/net/common/databaselib/SQLChiperHelper.kt
+++ databaselib/src/main/java/net/common/databaselib/SQLChiperHelper.kt
@@ -8,14 +8,12 @@ import net.sqlcipher.database.SQLiteDatabase
 import net.sqlcipher.database.SQLiteOpenHelper
 import kotlin.collections.ArrayList
 
-class SQLChiperHelper(context: Context?, dbName: String, dbVersion: Int, password: String, databaseListener: Listener.OnDatabaseListener) : SQLiteOpenHelper(context, dbName, null, dbVersion) {
+class SQLChiperHelper(context: Context?, dbName: String, dbVersion: Int, password: String, private var listener: Listener.OnDatabaseListener) : SQLiteOpenHelper(context, dbName, null, dbVersion) {
 
     var mdb: SQLiteDatabase
-    var listener: Listener.OnDatabaseListener? = null
 
     init {
         mdb = getWritableDatabase(password)
-        listener = databaseListener
     }
 
     override fun onCreate(db: SQLiteDatabase?) {
